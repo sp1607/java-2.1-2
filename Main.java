4c00839e814198f78a91624a14864c6e632077d9
sp1607 <77290138+sp1607@users.noreply.github.com>
2021-01-29 17:40:54 +0300

    Mod Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main (String [] args) {
         int balance = 100;
-        int top_up = 2000;
+        int topUp = 2000;
         int bonus;
-        if (top_up > 1000) {
-            bonus = top_up / 100;
+        if (topUp > 1000) {
+            bonus = topUp / 100;
         } else {
             bonus = 0;
         }
-        balance = balance + top_up + bonus;
+        balance = balance + topUp + bonus;
         System.out.println("Bonus = " + bonus);
         System.out.println("Balance = " + balance);
     }
